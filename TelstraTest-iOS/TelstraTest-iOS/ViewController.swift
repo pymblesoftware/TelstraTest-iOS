ca860dcf7c6f3d02d6c2a041a66a16b913e18d2d
Pymblesoftware <regan@pymblesoftware.com>
2018-02-15 21:18:23 +1100

    Created a basic iOS add

---
+++ TelstraTest-iOS/TelstraTest-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TelstraTest-iOS
+//
+//  Created by Regan Russell on 15/2/18.
+//  Copyright © 2018 Regan Russell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
